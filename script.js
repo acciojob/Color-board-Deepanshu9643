2ae166bf98796fb6978936f661ae23573bf158a7
Deepanshu9643 <deepanshukhorwal7@gmail.com>
2024-09-23 13:01:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,9 +6,12 @@ for(let i=0; i<800; i++){
 	square.addEventListener(`mouseover`, ()=>{
 		const color = `#${Math.floor(Math.random()*16777215).toString(16)}`
 		        square.style.backgroundColor = color 
+	})
+							square.addEventListener("mouseout",()=>{
 		setTimeout(()=>{
 			square.style.backgroundColor = "rgb(29, 29, 29)"
 		}, 800)
 	})
+	
 	container.appendChild(square)
 }
