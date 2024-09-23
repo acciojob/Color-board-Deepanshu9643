11abe181ce409b2684ac3d8659f12f7edd89cef0
Deepanshu9643 <deepanshukhorwal7@gmail.com>
2024-09-23 12:48:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const container = document.querySelector(".container");
+for(let i=0; i<800; i++){
+	const square = document.createElement("div")
+	square.classList.add("square")
+	square.addEventListener(`mouseover`, ()=>{
+		const color = `#${Math.floor(Math.random()*16777215).toString(16)}`
+		        square.style.backgroundColor = color 
+		setTimeout(()=>{
+			square.style.backgroundColor = "black"
+		}, 800)
+	})
+	container.appendChild(square)
+}
